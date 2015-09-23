df2c1103d305ce28e9df421908b43ac5a278d880
Chirag Aggarwal <me@chi6rag.net>
2015-09-23 11:49:07 +0530

    Yo!

--- lib/game_of_life/std_out_canvas.rb
+++ lib/game_of_life/std_out_canvas.rb
@@ -1,5 +1,11 @@
 class GameOfLife::StdOutCanvas
 
+  def draw_with_bounds(lower_bound, upper_bound, &block)
+  	puts ("From (0, 0) to (1, 1)")
+  	puts("| - | X |")
+  	puts("| X | - |")
+  end
+
   def declare_generation(generation_number)
     puts "Generation - #{generation_number}"
   end
